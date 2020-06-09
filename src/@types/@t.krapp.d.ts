ec5533be12462c8bdb99e8f5fd03ff0aacf54213
Thomas Krapp <t.krapp@gmail.com>
2020-06-09 22:33:54 +0200

    [sudoku] add even more features!

--- src/@types/@t.krapp.d.ts
+++ src/@types/@t.krapp.d.ts
@@ -1,9 +1,18 @@
-declare function solve(game: string, start_at: number): IterableIterator<string>;
-declare function check_item(item: string): Boolean;
-declare function get_col(game: string, col_number: number): string;
-declare function get_row(game: string, row_number: number): string;
-declare function get_square(game: string, x: number, y: number): string;
+declare module "@t.krapp/sudoku-js" {
+    function solve(game: string, start_at: Number): IterableIterator<string>;
+    function check_item(item: string): Boolean;
+    function get_col(game: string, col_number: Number): string;
+    function get_row(game: string, row_number: Number): string;
+    function get_square(game: string, x: Number, y: Number): string;
+    function get_candidates(game: string, x: Number, y: Number): Set<string>;
+    function difference(setA: Set<string>, setB: Set<string>): Set<string>;
+    function is_complete(game: string): Boolean;
+    function* iter_squares(game: string): IterableIterator<string>;
+    function* iter_rows(game: string): IterableIterator<string>;
+    function* iter_cols(game: string): IterableIterator<string>;
+    function* range(start: Number, stop: Number, step: Number): IterableIterator<Number>;
 
-declare class ItemError {
-    constructor(message: string);
+    class ItemError {
+        constructor(message: string);
+    }
 }
--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,16 +1,40 @@
-import { check_item, get_col, get_row, get_square, solve } from "@t.krapp/sudoku-js";
+import {
+    check_item,
+    get_col,
+    get_row,
+    get_square,
+    solve,
+} from "@t.krapp/sudoku-js";
 
+const VALUE_SPACE = " ";
+
+const EMPTY_GAME = VALUE_SPACE.repeat(81);
+
+const POSSIBLE_VALUES = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const ROW_SIZE = POSSIBLE_VALUES.length;
 
 const CLASS_INPUT = "input-container";
-const EMPTY_GAME = " ".repeat(81);
-const POSSIBLE_VALUES = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const ROW_SIZE = POSSIBLE_VALUES.length;
-const CLASS_INPUT_SELECTED = "input-container-selected";
+const CLASS_INPUT_SELECTED = "selected";
+const CLASS_INPUT_HIGHTLIGHTED = "highlighted";
+const CLASS_INPUT_ERRORED = "errored";
+const CLASS_INPUT_FILLED = "filled";
+const CLASS_BTN_INFO = "btn-outline-info";
+const CLASS_BTN_LIGHT = "btn-outline-light";
+
+const ATTR_FIELD_INDEX = "data-field-index";
+const ATTR_READONLY = "data-readonly";
+const ATTR_TABINDEX = "tabindex";
+
+const EVENT_TYPE_CLICK = "click";
+const EVENT_TYPE_FOCUS = "focus";
+const EVENT_TYPE_KEYUP = "keyup";
 
 class SudokuGame {
     solution: string;
     game: string;
     elements: Array<HTMLElement>;
+    controls: NodeListOf<HTMLButtonElement>;
     protected selectedElement: keyof Array<HTMLElement>;
 
     constructor(gameElement: HTMLElement, controlsElement: HTMLElement) {
@@ -20,7 +44,9 @@ class SudokuGame {
         console.log("Got game");
         this.game = this.clearSomeFields(this.solution, 50);
         console.log("Removed fields");
-        this.elements = Array.prototype.map.call(this.game, this.generateElement.bind(this));
+        this.elements = this.game
+            .split("")
+            .map(this.generateElement.bind(this));
 
         this.drawGame();
 
@@ -29,14 +55,27 @@ class SudokuGame {
         }
 
         gameElement.style.height = `${gameElement.offsetWidth}px`;
-        gameElement.addEventListener("keyup", this.setFieldValueViaKeyboard.bind(this));
+        gameElement.addEventListener(
+            EVENT_TYPE_KEYUP,
+            this.setFieldValueViaKeyboard.bind(this)
+        );
 
-        controlsElement.querySelectorAll("button[value]").forEach((buttonElement: HTMLButtonElement) => {
-            buttonElement.addEventListener("click", this.setFieldValueViaButton.bind(this));
-        });
-        controlsElement.querySelector("button.btn-fullscreen").addEventListener("click", function (evt) {
-            document.documentElement.requestFullscreen();
-        });
+        controlsElement
+            .querySelectorAll<HTMLButtonElement>("button[value]")
+            .forEach((buttonElement: HTMLButtonElement) => {
+                buttonElement.addEventListener(
+                    EVENT_TYPE_CLICK,
+                    this.setFieldValueViaButton.bind(this)
+                );
+            });
+        controlsElement
+            .querySelector("button.btn-fullscreen")!
+            .addEventListener(EVENT_TYPE_CLICK, function (evt) {
+                document.documentElement.requestFullscreen();
+            });
+
+        this.controls = controlsElement.querySelectorAll("button[value]");
+        this.selectedElement = this.game.indexOf(VALUE_SPACE);
     }
 
     getXfromPosition(position: number): number {
@@ -55,19 +94,19 @@ class SudokuGame {
             let position: number;
             do {
                 position = this.getRandomNumber(0, game.length);
-            } while (game[position] === " ");
+            } while (game[position] === VALUE_SPACE);
             let solutionIterator;
 
             newGame = this.replaceAtPosition(
-                this.replaceAtPosition(game, position, " "),
+                this.replaceAtPosition(game, position, VALUE_SPACE),
                 game.length - position - 1,
-                " "
+                VALUE_SPACE
             );
             solutionIterator = solve(newGame, 0);
 
             solutionIterator.next();
             moreThanOneSolution = !solutionIterator.next().done;
-        } while (moreThanOneSolution)
+        } while (moreThanOneSolution);
 
         if (num_fields > 0) {
             return this.clearSomeFields(newGame, num_fields - 2);
@@ -80,13 +119,13 @@ class SudokuGame {
 
         value = value.trim();
 
-        divElement.setAttribute("data-field-index", index.toString());
+        divElement.setAttribute(ATTR_FIELD_INDEX, index.toString());
 
         if (value !== "") {
-            divElement.setAttribute("data-readonly", "true");
+            divElement.setAttribute(ATTR_READONLY, "true");
         } else {
-            divElement.setAttribute("tabindex", "0");
-            divElement.addEventListener("focus", this.selectField.bind(this));
+            divElement.setAttribute(ATTR_TABINDEX, "0");
+            divElement.addEventListener(EVENT_TYPE_FOCUS, this.selectField.bind(this));
         }
 
         divElement.classList.add(CLASS_INPUT);
@@ -97,15 +136,16 @@ class SudokuGame {
     drawGame() {
         let self = this;
 
-        Array.prototype.forEach.call(
-            this.game,
-            function (item: string, index: number) {
-                let element = self.elements[index];
+        Array.prototype.forEach.call(this.game, function (
+            item: string,
+            index: number
+        ) {
+            let element = self.elements[index];
 
-                if (element.innerHTML !== item) {
-                    element.innerHTML = item;
-                }
-            });
+            if (element.innerHTML !== item) {
+                element.innerHTML = item;
+            }
+        });
     }
 
     occurresOnce(char: string, item: string): Boolean {
@@ -113,63 +153,113 @@ class SudokuGame {
     }
 
     selectField(evt: Event) {
-        let element = <HTMLElement> evt.target;
+        let element = <HTMLElement>evt.target;
 
         if (this.selectedElement !== undefined) {
-            let previousElement = <HTMLElement> this.elements[this.selectedElement];
+            let previousElement = <HTMLElement>(
+                this.elements[this.selectedElement]
+            );
             previousElement.classList.remove(CLASS_INPUT_SELECTED);
         }
 
         element.classList.add(CLASS_INPUT_SELECTED);
 
-        this.selectedElement = parseInt(element.getAttribute("data-field-index"), 10);
+        this.selectedElement = parseInt(
+            element.getAttribute(ATTR_FIELD_INDEX)!,
+            10
+        );
+        this.highlightFields();
+    }
+
+    highlightFields() {
+        let highlightValue = this.game[this.selectedElement as number];
+
+        this.elements.forEach((element: HTMLElement) => {
+            element.classList.remove(CLASS_INPUT_HIGHTLIGHTED);
+        });
+
+        if (highlightValue === VALUE_SPACE) {
+            return;
+        }
+
+        this.game.split("").forEach((value: string, index: number) => {
+            if (value === highlightValue) {
+                this.elements[index].classList.add(CLASS_INPUT_HIGHTLIGHTED);
+            }
+        });
+    }
+
+    updateControls() {
+        this.controls.forEach((element: HTMLButtonElement) => {
+            let value = element.value;
+
+            if (this.game.split(value).length - 1 === 9) {
+                element.classList.remove(CLASS_BTN_INFO);
+                element.classList.add(CLASS_BTN_LIGHT);
+            } else {
+                element.classList.remove(CLASS_BTN_LIGHT);
+                element.classList.add(CLASS_BTN_INFO);
+            }
+        });
     }
 
     setFieldValue(value: string) {
         if (this.selectedElement === undefined) {
             return;
         }
-        let element = <HTMLElement> this.elements[this.selectedElement];
-        let position = <number> this.selectedElement;
+        let element = <HTMLElement>this.elements[this.selectedElement];
+        let position = <number>this.selectedElement;
         let errorOccurred = false;
         let x = this.getXfromPosition(position);
         let y = this.getYfromPosition(position);
 
+        if (this.game[position] === value) {
+            value = VALUE_SPACE;
+        }
+
         this.game = this.replaceAtPosition(this.game, position, value);
 
-        if (!this.occurresOnce(value, get_col(this.game, x))) {
-            element.classList.add('error');
-            errorOccurred = true;
-        }
+        if (value !== VALUE_SPACE) {
+            element.classList.add(CLASS_INPUT_FILLED);
 
-        if (!this.occurresOnce(value, get_row(this.game, y))) {
-            element.classList.add('error');
-            errorOccurred = true;
-        }
+            if (!this.occurresOnce(value, get_col(this.game, x))) {
+                element.classList.add(CLASS_INPUT_ERRORED);
+                errorOccurred = true;
+            }
 
-        if (!this.occurresOnce(value, get_square(this.game, x, y))) {
-            element.classList.add('error');
-            errorOccurred = true;
+            if (!this.occurresOnce(value, get_row(this.game, y))) {
+                element.classList.add(CLASS_INPUT_ERRORED);
+                errorOccurred = true;
+            }
+
+            if (!this.occurresOnce(value, get_square(this.game, x, y))) {
+                element.classList.add(CLASS_INPUT_ERRORED);
+                errorOccurred = true;
+            }
+        } else {
+            element.classList.remove(CLASS_INPUT_FILLED);
         }
 
         if (errorOccurred === false) {
             console.log(errorOccurred);
-            element.classList.remove("error");
+            element.classList.remove(CLASS_INPUT_ERRORED);
         }
 
+        this.highlightFields();
+        this.updateControls();
         this.drawGame();
     }
 
     setFieldValueViaKeyboard(evt: KeyboardEvent) {
         if (POSSIBLE_VALUES.indexOf(evt.key) >= 0 || evt.key === "Backspace") {
-            let value = evt.key === "Backspace" ? " " : evt.key;
+            let value = evt.key === "Backspace" ? VALUE_SPACE : evt.key;
 
             this.setFieldValue(value);
         }
     }
 
     setFieldValueViaButton(evt: MouseEvent) {
-        let buttonElement = <HTMLButtonElement> evt.target;
+        let buttonElement = <HTMLButtonElement>evt.target;
         let value = buttonElement.value;
 
         this.setFieldValue(value);
@@ -193,13 +283,18 @@ class SudokuGame {
     }
 
     replaceAtPosition(game: string, position: number, replace: string): string {
-        return game.substring(0, position) + replace + game.substring(position + 1);
+        return (
+            game.substring(0, position) + replace + game.substring(position + 1)
+        );
     }
 }
 
 function initSite() {
-    document.body.style.height = `${window.screen.height}px`;
+    // document.body.style.height = `${window.screen.height}px`;
 }
 
-new SudokuGame(document.querySelector(".game"), document.querySelector('.controls'));
+new SudokuGame(
+    document.querySelector<HTMLElement>(".game")!,
+    document.querySelector<HTMLElement>(".controls")!
+);
 initSite();
