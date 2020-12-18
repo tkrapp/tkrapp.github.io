b452637f1165e3bf3521b1aa27617e89ae407bae
Thomas Krapp <t.krapp@gmail.com>
2020-12-18 20:06:16 +0100

    Update type hints

--- src/js/@types/@t.krapp.d.ts
+++ src/js/@types/@t.krapp.d.ts
@@ -1,12 +1,12 @@
 declare module "@t.krapp/sudoku-js" {
     function solve(game: string, start_at: number): IterableIterator<string>;
-    function check_item(item: string): Boolean;
+    function check_item(item: string): boolean;
     function get_col(game: string, col_number: number): string;
     function get_row(game: string, row_number: number): string;
     function get_square(game: string, x: number, y: number): string;
     function get_candidates(game: string, x: number, y: number): Set<string>;
     function difference(setA: Set<string>, setB: Set<string>): Set<string>;
-    function is_complete(game: string): Boolean;
+    function is_complete(game: string): boolean;
     function* iter_squares(game: string): IterableIterator<string>;
     function* iter_rows(game: string): IterableIterator<string>;
     function* iter_cols(game: string): IterableIterator<string>;
