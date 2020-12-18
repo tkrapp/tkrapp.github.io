223a0464914916628ec102f57d0efcf016b43e91
Thomas Krapp <t.krapp@gmail.com>
2020-12-18 19:56:45 +0100

    Update type hints

--- src/js/@types/@t.krapp.d.ts
+++ src/js/@types/@t.krapp.d.ts
@@ -1,16 +1,16 @@
 declare module "@t.krapp/sudoku-js" {
-    function solve(game: string, start_at: Number): IterableIterator<string>;
+    function solve(game: string, start_at: number): IterableIterator<string>;
     function check_item(item: string): Boolean;
-    function get_col(game: string, col_number: Number): string;
-    function get_row(game: string, row_number: Number): string;
-    function get_square(game: string, x: Number, y: Number): string;
-    function get_candidates(game: string, x: Number, y: Number): Set<string>;
+    function get_col(game: string, col_number: number): string;
+    function get_row(game: string, row_number: number): string;
+    function get_square(game: string, x: number, y: number): string;
+    function get_candidates(game: string, x: number, y: number): Set<string>;
     function difference(setA: Set<string>, setB: Set<string>): Set<string>;
     function is_complete(game: string): Boolean;
     function* iter_squares(game: string): IterableIterator<string>;
     function* iter_rows(game: string): IterableIterator<string>;
     function* iter_cols(game: string): IterableIterator<string>;
-    function* range(start: Number, stop: Number, step: Number): IterableIterator<Number>;
+    function* range(start: number, stop: number, step: number): IterableIterator<number>;
 
     class ItemError {
         constructor(message: string);
